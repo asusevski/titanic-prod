f8637ed19500948fb0d7edbf5e3fcf11d388ac52
Anthony <asusevski@gmail.com>
2022-11-03 21:19:47 -0400

    training script updated

--- train.py
+++ train.py
@@ -1,19 +1,17 @@
-import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit, train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 import wandb
 import xgboost as xgb
-from wandb.xgboost import wandb_callback
+from wandb.integration.xgboost import WandbCallback
 
 
-run = wandb.init(project="my-test-project")
+run = wandb.init(project="my-test-project-2")
 
 
 df_train = pd.read_csv('./data/train.csv')
@@ -106,64 +104,37 @@ df_train_split_preprocessed = pipe.fit_transform(train_split)
 df_test_split_preprocessed = pipe.fit_transform(test_split)
 
 X_train = df_train_split_preprocessed.drop(columns=['Survived'])
-y_train = df_train_split_preprocessed["Survived"]
+y_train = df_train_split_preprocessed[["Survived"]]
 
 X_test = df_test_split_preprocessed.drop(columns=['Survived'])
-y_test = df_test_split_preprocessed["Survived"]
+y_test = df_test_split_preprocessed[["Survived"]]
 
 
-# WandB
-xg_train = xgb.DMatrix(X_train, label=y_train)
-xg_test = xgb.DMatrix(X_test, label=y_test)
-# setup parameters for xgboost
-# param = {}
-# # use logistic binary classification
-# param['objective'] = 'binary:logistic'
-# # scale weight of positive examples
-# param['eta'] = 0.1
-# param['max_depth'] = 6
-# param['silent'] = 1
-# param['nthread'] = 4
-# param['num_class'] = 2
-param = {
-        'objective': 'binary:logistic'
-        , 'gamma': 1               ## def: 0
-        , 'learning_rate': 0.1     ## def: 0.1
-        , 'max_depth': 3
-        , 'min_child_weight': 100  ## def: 1
-        , 'n_estimators': 25
-        , 'nthread': 24 
-        , 'random_state': 42
-        , 'reg_alpha': 0
-        , 'reg_lambda': 0          ## def: 1
-        , 'eval_metric': ['auc', 'logloss']
-        , 'tree_method': 'hist'  # use gpu_hist to train on GPU
-    }
-#wandb.config.update(param)
-run.config.update(dict(param))
-xgbmodel = xgb.XGBClassifier(**param, use_label_encoder=False)
-xgbmodel.fit(X_train, y_train, eval_set=[(X_test, y_test)], callbacks=[wandb_callback()])
-bstr = xgbmodel.get_booster()
-# Save the booster to disk
-model_name = f'{run.name}_model.json'
-model_path = model_dir/model_name
-bstr.save_model(str(model_path))
+config = wandb.config
+config.seed = 123
 
-# Get the booster's config
-config = json.loads(bstr.save_config())
+config.test_size = 0.2
+config.colsample_bytree = 0.3
+config.learning_rate = 0.01
+config.max_depth = 15
+config.alpha = 10
+config.n_estimators = 5
 
-# Log the trained model to W&B Artifacts, including the booster's config
-model_art = wandb.Artifact(name=model_name, type='model', metadata=dict(config))
-model_art.add_file(model_path)
-run.log_artifact(model_art)
+wandb.config.update(config)
 
+xg_reg = xgb.XGBClassifier(objective='binary:logistic', 
+    colsample_bytree=config.colsample_bytree, 
+    learning_rate=config.learning_rate,
+    max_depth=config.max_depth, 
+    alpha=config.alpha, 
+    n_estimators=config.n_estimators)
 
-# watchlist = [(xg_train, 'train'), (xg_test, 'test')]
-# num_round = 5
-# bst = xgb.train(params=param, dtrain=xg_train, evals=watchlist, num_boost_round=num_round,callbacks=[wandb_callback()])
-# #bst = xgb.train(param, xg_train, num_round, watchlist, callbacks=[wandb_callback()])
-# # get prediction
-# pred = bst.predict(xg_test)
-# error_rate = np.sum(pred != y_test) / y_test.shape[0]
-# print(f'Test error using logistic = {error_rate}')
-# wandb.summary['Error Rate'] = error_rate
+xg_reg.fit(X_train,y_train,
+           callbacks=[WandbCallback()]
+)
+
+preds = xg_reg.predict(X_test)
+
+run.summary["test_score"] = accuracy_score(y_test, preds)
+
+wandb.finish()
